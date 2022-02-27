42ea6a155586267b73e4a423210c2d4b3fa99f7d
권지원 <jiwonk419@11st.co.kr>
2022-02-27 17:00:53 +0900

    project init

---
+++ src/main/java/com/zijigo/wwwmeet/WwwmeetApplication.java
@@ -0,0 +1,13 @@
+package com.zijigo.wwwmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwmeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwmeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zijigo/wwwmeet/WwwmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zijigo.wwwmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwmeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
